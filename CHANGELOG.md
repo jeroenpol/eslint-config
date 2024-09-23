e5838a9508fcddf4fc952ee692f71106c3951dd9
Jeroen Pol <jeroenpol3@gmail.com>
2024-09-23 10:55:36 +0200

    feat(eslint): stylystic config

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+1.0.11 / 2024-09-18
+==================
+ - [minor] Utilize base configurations
+
 1.0.10 / 2024-09-16
 ==================
  - [minor] Cleaned configuration and moved it to one file
--- index.js
+++ index.js
@@ -1,20 +1,23 @@
+// TODO:
+// difference between constructor inject and inject()
+// check preset for stylistic
+// check preset for typescript-eslint
+// TODO: add rule that you have to assign reduce, map to a variable
+
 module.exports = {
   plugins: ['@typescript-eslint'],
-  rules: {
-    '@typescript-eslint/explicit-member-accessibility': 'off',
-  },
+  rules: { '@typescript-eslint/explicit-member-accessibility': 'off' },
   overrides: [
     {
-      files: ['*.ts', '*.tsx'],
+      files: ['*.ts', '*.tsx', '*.js'],
       parser: '@typescript-eslint/parser',
       parserOptions: {
         ecmaVersion: 2020,
         sourceType: 'module',
-        // tsconfigRootDir: __dirname,
         project: ['tsconfig.json'],
       },
       plugins: [
-        '@stylistic', 
+        '@stylistic',
         'rxjs-angular',
         'sonarjs',
         'simple-import-sort',
@@ -25,7 +28,7 @@ module.exports = {
         'no-unreachable': 'error',
         'no-debugger': 'error',
         'no-console': 'error',
-        'no-empty': 'error',                                    // explain
+        'no-empty': 'error',
         'unicode-bom': 'warn',
 
         // NAMING CONVENTIONS
@@ -33,7 +36,11 @@ module.exports = {
           'warn',
           {
             selector: 'enum',
-            format: ['PascalCase', 'UPPER_CASE'],
+            format: ['PascalCase'],
+          },
+          {
+            selector: 'enumMember',
+            format: ['UPPER_CASE'],
           },
           {
             selector: 'interface',
@@ -42,14 +49,14 @@ module.exports = {
         ],
 
         // STYLISTIC
-        '@stylistic/quote-props': ['warn', 'as-needed'],
+        // '@stylistic/quote-props': ['warn', 'as-needed'],
         '@stylistic/array-bracket-newline': ['warn', 'consistent'],
-        '@stylistic/array-bracket-spacing': ['warn', 'never'],
+        // '@stylistic/array-bracket-spacing': ['warn', 'never'],
         '@stylistic/array-element-newline': ['warn', 'consistent'],
-        '@stylistic/arrow-parens': 'warn',
-        '@stylistic/arrow-spacing': 'warn',
-        '@stylistic/block-spacing': 'warn',
-        '@stylistic/brace-style': 'warn',
+        // '@stylistic/arrow-parens': 'warn',
+        // '@stylistic/arrow-spacing': 'warn',
+        // '@stylistic/block-spacing': 'warn',
+        // '@stylistic/brace-style': 'warn',
         '@stylistic/comma-dangle': [
           'warn',
           {
@@ -60,79 +67,78 @@ module.exports = {
             functions: 'never',
           },
         ],
-        '@stylistic/comma-spacing': 'warn',
-        '@stylistic/computed-property-spacing': 'warn',
-        '@stylistic/dot-location': ['warn', 'object'],
-        '@stylistic/eol-last': 'warn',
+        // '@stylistic/comma-spacing': 'warn',
+        // '@stylistic/computed-property-spacing': 'warn',
+        '@stylistic/dot-location': ['warn', 'property'],
+        // '@stylistic/eol-last': 'warn',
         '@stylistic/function-call-spacing': ['error', 'never'],
         '@stylistic/function-call-argument-newline': ['warn', 'consistent'],
         '@stylistic/function-paren-newline': ['warn', 'multiline-arguments'],
-        '@stylistic/generator-star-spacing': ['error', 'after'],
+        // '@stylistic/generator-star-spacing': ['error', 'after'],
         '@stylistic/implicit-arrow-linebreak': ['error', 'beside'],
-        '@stylistic/indent': [
-          'warn',
-          2,
-          {
-            SwitchCase: 1,
-            ObjectExpression: 1,
-            offsetTernaryExpressions: true,
-            ImportDeclaration: 'first',
-            FunctionDeclaration: { parameters: 'first', body: 1 },
-            CallExpression: { arguments: 'first' },
-          },
-        ],
-        '@stylistic/key-spacing': ['warn'],
-        '@stylistic/keyword-spacing': ['warn', { after: true, before: true }],
+        // '@stylistic/indent': [
+        //   'warn',
+        //   2,
+        //   {
+        //     SwitchCase: 1,
+        //     ObjectExpression: 1,
+        //     offsetTernaryExpressions: true,
+        //     ImportDeclaration: 'first',
+        //     FunctionDeclaration: { parameters: 'first', body: 1 },
+        //     CallExpression: { arguments: 'first' },
+        //   },
+        // ],
+        // '@stylistic/key-spacing': ['warn'],
+        // '@stylistic/keyword-spacing': ['warn', { after: true, before: true }],
         '@stylistic/max-len': [
           'error',
           {
-            code: 120, // TO DISCUSS
+            code: 120,
             ignoreRegExpLiterals: true,
             ignoreStrings: true,
             ignoreTemplateLiterals: true,
             ignoreUrls: true,
           },
         ],
-        '@stylistic/member-delimiter-style': ['warn'],
-        '@stylistic/multiline-ternary': ['warn', 'always'], // TO DISCUSS
-        '@stylistic/new-parens': 'warn',
-        '@stylistic/no-confusing-arrow': 'warn',
+        // '@stylistic/member-delimiter-style': ['warn'],
+        // '@stylistic/new-parens': 'warn',
+        // '@stylistic/no-confusing-arrow': 'warn',
         '@stylistic/no-extra-semi': 'warn',
-        '@stylistic/no-mixed-operators': 'error',
-        '@stylistic/no-mixed-spaces-and-tabs': 'warn',
-        '@stylistic/no-multi-spaces': 'warn',
-        '@stylistic/no-multiple-empty-lines': ['warn', { max: 1 }],
-        '@stylistic/no-tabs': 'error',
-        '@stylistic/no-trailing-spaces': 'warn',
-        '@stylistic/no-whitespace-before-property': 'warn',
-        '@stylistic/nonblock-statement-body-position': 'warn',
-        '@stylistic/object-curly-newline': ['error', { 'consistent': true }],
-        '@stylistic/object-curly-spacing': ['warn', 'always'],
-        '@stylistic/object-property-newline': ['warn', { allowAllPropertiesOnSameLine: true }],
-        '@stylistic/one-var-declaration-per-line': 'warn',
-        '@stylistic/operator-linebreak': ['warn', 'after', { overrides: { '?': 'before', ':': 'before' } }], // TO DISCUSS
-        '@stylistic/padded-blocks': ['warn', { blocks: 'never', switches: 'never' }],
-        '@stylistic/quotes': ['warn', 'single', { avoidEscape: true, allowTemplateLiterals: true }],
-        '@stylistic/rest-spread-spacing': ['warn', 'never'],
-        '@stylistic/semi': ['warn', 'always'],
-        '@stylistic/semi-spacing': 'warn',
+        // '@stylistic/no-mixed-operators': 'error',
+        // '@stylistic/no-mixed-spaces-and-tabs': 'warn',
+        // '@stylistic/no-multi-spaces': 'warn',
+        // '@stylistic/no-multiple-empty-lines': ['warn', { max: 1 }],
+        // '@stylistic/no-tabs': 'error',
+        // '@stylistic/no-trailing-spaces': 'warn',
+        // '@stylistic/no-whitespace-before-property': 'warn',
+        // '@stylistic/nonblock-statement-body-position': 'warn',
+        '@stylistic/object-curly-newline': ['error', { consistent: true }],
+        // '@stylistic/object-curly-spacing': ['warn', 'always'],
+        '@stylistic/object-property-newline': ['error', { allowAllPropertiesOnSameLine: true }],
+        // '@stylistic/one-var-declaration-per-line': 'warn',
+        // '@stylistic/operator-linebreak': ['warn', 'after', { overrides: { '?': 'before', ':': 'before' } }],
+        // '@stylistic/padded-blocks': ['warn', { blocks: 'never', switches: 'never' }],
+        // CHECK DEFAULT VALUE IN CODE
+        // '@stylistic/quotes': ['warn', 'single', { avoidEscape: true, allowTemplateLiterals: true }],
+        // '@stylistic/rest-spread-spacing': ['warn', 'never'],
+        // '@stylistic/semi': ['warn', 'always'],
+        // '@stylistic/semi-spacing': 'warn',
         '@stylistic/semi-style': 'warn',
-        '@stylistic/wrap-iife': 'warn',
-        '@stylistic/space-before-blocks': 'warn',
-        '@stylistic/space-before-function-paren': ['warn', { anonymous: 'always', named: 'never', asyncArrow: 'always' }],
-        '@stylistic/space-in-parens': ['warn', 'never'],
-        '@stylistic/space-infix-ops': 'warn',
-        '@stylistic/space-unary-ops': 'warn',
+        // '@stylistic/wrap-iife': 'warn',
+        // '@stylistic/space-before-blocks': 'warn',
+        // '@stylistic/space-before-function-paren': ['warn', {
+        // anonymous: 'always', named: 'never', asyncArrow: 'always'
+        // }],
+        // '@stylistic/space-in-parens': ['warn', 'never'],
+        // '@stylistic/space-infix-ops': 'warn',
+        // '@stylistic/space-unary-ops': 'warn',
         '@stylistic/switch-colon-spacing': 'warn',
-        '@stylistic/template-curly-spacing': ['warn', 'never'],
-        'curly': ['error', 'all'],
-        'no-unexpected-multiline': 'error',
+        // '@stylistic/template-curly-spacing': ['warn', 'never'],
 
         // CODE COMPLEXITY
         'max-depth': ['error', 8],
         'max-nested-callbacks': ['error', 8],
-        'max-statements-per-line': ['error', { 'max': 1 }],
-        'complexity': ['error', { max: 15 }],
+        'max-statements-per-line': ['error', { max: 1 }],
         'sonarjs/cognitive-complexity': ['error', 15],
 
         // CLASSES
@@ -169,26 +175,29 @@ module.exports = {
         ],
 
         // FUNCTIONS
-        '@typescript-eslint/explicit-function-return-type': ['error', { allowHigherOrderFunctions: true }],
-        'max-lines-per-function': ['error', { 'max': 60, 'skipBlankLines': true, 'skipComments': true }],
-        'max-statements': ['error', 20],                          // can be lowered
+        'curly': ['error', 'all'],
+        'max-lines-per-function': ['error', { max: 60, skipBlankLines: true, skipComments: true }],
+        'max-statements': ['error', 20],
+        'no-empty-function': 'error',
         'no-param-reassign': 'error',
-        'sonarjs/no-extra-arguments': 'error',
+        'no-unexpected-multiline': 'error',
         'prefer-arrow-callback': 'warn',
+        '@typescript-eslint/explicit-function-return-type': ['error', { allowHigherOrderFunctions: true }],
+        'sonarjs/no-extra-arguments': 'error',
 
         // VARIABLES
-        "unused-imports/no-unused-vars": [
-          "error",
-          { "vars": "all", "varsIgnorePattern": "^_", "args": "after-used", "argsIgnorePattern": "^_" }
+        'unused-imports/no-unused-vars': [
+          'error',
+          { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
         ],
-        'no-shadow': 'error',                                     // explain
+        'no-shadow': 'error',
         'no-multi-assign': 'error',
         'prefer-const': 'error',
         'no-unused-expressions': 'error',
         'sonarjs/non-existent-operator': 'error',
         '@typescript-eslint/no-explicit-any': 'warn',
         '@typescript-eslint/no-non-null-assertion': 'error',
-        '@typescript-eslint/no-inferrable-types': 'error',        // explain
+        '@typescript-eslint/no-inferrable-types': 'error',
         '@typescript-eslint/ban-types': [
           'error',
           {
@@ -213,10 +222,10 @@ module.exports = {
             },
           },
         ],
-        '@typescript-eslint/prefer-optional-chain': 'warn',       // explain
-        
+        '@typescript-eslint/prefer-optional-chain': 'warn',
+
         // ARRAYS
-        'array-callback-return': 'error',                         // explain
+        'array-callback-return': 'error',
         '@typescript-eslint/array-type': 'error',
         'no-array-constructor': 'error',
         '@typescript-eslint/prefer-includes': 'warn',
@@ -231,16 +240,31 @@ module.exports = {
         'sonarjs/no-inverted-boolean-check': 'error',
         'eqeqeq': 'error',
 
-        // RxJS
-        'rxjs-angular/prefer-takeuntil': [
+        // ASYNCHRONOUS
+        '@typescript-eslint/no-misused-promises': 'error',
+        '@typescript-eslint/no-floating-promises': 'error',
+        'rxjs-angular/prefer-takeuntil': [ // check if this one is needed (first, take(1))
           'error',
           {
-            'alias': ['untilDestroyed'],
-            'checkComplete': true,
-            'checkDecorators': ['Component'],
-            'checkDestroy': true
-          }
+            alias: ['takeUntilDestroyed'],
+            checkComplete: true,
+            checkDecorators: ['Component'],
+            checkDestroy: true,
+          },
         ],
+      },
+    },
+    {
+      files: ['*.spec.ts'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+      plugins: [],
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        'max-lines-per-function': 'off',
       },
     },
   ],
